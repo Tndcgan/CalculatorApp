aea0f441261528a53d1747c3490365b43a4a04b4
Tndcgan <organoppo@gmail.com>
2024-10-25 16:00:26 +0700

    nee class

---
+++ CalculatorApp/class/class.cs
@@ -0,0 +1,15 @@
+namespace CalculatorApp.class;
+
+public class class : ContentPage
+{
+	public class()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
